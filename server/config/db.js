348c0b6a07ea2c6176f0709738bd9257828200df
AliGoharbakhsh <agoharba@my.centennialcollege.ca>
2020-10-22 01:45:05 -0400

    updated db URI to work with MongoDB

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/book_store"
+   // "URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://ali:a80SB4OqYCaXg6wv@mongodbserver.ut7cn.mongodb.net/book_store?retryWrites=true&w=majority"
 
 }
